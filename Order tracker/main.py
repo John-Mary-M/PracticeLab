e1369e5e9c38279822b3c7109bbba083587d9ef8
mukisa <mukisa@Ubuntu20.4.myguest.virtualbox.org>
2023-08-08 21:24:07 +0300

    writing and order tracking app

---
+++ Order tracker/main.py
@@ -0,0 +1,13 @@
+"""This module is where the app will run"""
+import features
+import cmd
+
+def main():
+    pass
+
+if __name__:="__main__":
+    features.Base.do_load_data(main)
+    features.Base().cmdloop()
+    
+    
+    
