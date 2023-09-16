af350a46d53611c1a5005514c8cd17dced88a88c
John-Mary-M <johnmaryluxin8@gmail.com>
2023-09-16 06:14:50 +0300

    Adeiu.py taught me how bad an extra white space can be hard to detect

--- cs50P/adieu/adieu.py
+++ cs50P/adieu/adieu.py
@@ -1,13 +1,25 @@
 '''cs50p 13/09/2023 problemset 4 adieu.py'''
+import inflect
+import sys
+
+# Create an instance of the inflect engine
+p = inflect.engine()
+
+names = []
 while True:
     try:
-        usr_input = input("Name: ")
-        if ' ' in usr_input:
-            lst_names = usr_input.split(' ')
-            for name in lst_names[ : -1]:
-                name = name + ', '
-                print(f'Adieu, adieu, to {name}')
-        else:
-            print(f'Adieu, adieu, to {name}')
+        name = input("Name: ")
+        if not name:
+            break
+        names.append(name)
     except EOFError:
+        print()
         break
+n = len(names)
+if n == 1:
+    farewell = f"Adieu, adieu, to {p.join(names, ', ')}"
+else:
+    farewell = f"Adieu, adieu, to {p.join(names, ',')}"
+
+print(farewell)
+sys.exit(0)
