a0962c45b4f385cc3484e1a66ee803a4c99bc3ed
John-Mary-M <johnmaryluxin8@gmail.com>
2023-10-03 05:08:33 +0300

    cleaned up unecesary init file

--- cs50P/twttr/twttr.py
+++ cs50P/twttr/twttr.py
@@ -5,11 +5,12 @@ def main():
     # prompt user for input
     text = input('Input: ').strip()
     new_txt = ''
-    new_txt = remove_vowels(text)
+    new_txt = shorten(text)
     # output same text with all vowels omited
     print(f'Output: {new_txt}')
 # list of chars to omit
-def remove_vowels(text):
+def shorten(word):
     vowel_lst = ['a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U']
-    return ''.join(char for char in text if char not in vowel_lst)
-main()
+    return ''.join(char for char in word if char not in vowel_lst)
+if __name__ == "__main__":
+    main()
