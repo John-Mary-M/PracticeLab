9d192d79b3b598fb1a1da19835ac24395e78cad0
John-Mary-M <johnmaryluxin8@gmail.com>
2023-10-15 13:42:00 +0300

    fixxed csvDict writer bugs

--- cs50P/scourgify/scourgify.py
+++ cs50P/scourgify/scourgify.py
@@ -5,15 +5,13 @@ import csv
 
 
 def main():
-    """Entry point"""
+    """Entry point of the program"""
     if check_cmd_args():
         read_csv(sys.argv[1])
-        # rtn_lst = read_csv(sys.argv[1])
-        # for i in rtn_lst:
-        #     print(i)
+
 
 def check_cmd_args():
-    """Checks presence of proper cmd-line arguments"""
+    """Checks the presence of proper command-line arguments"""
     if len(sys.argv) < 3:
         sys.exit("Too few command-line arguments")
     if len(sys.argv) > 3:
@@ -21,39 +19,40 @@ def check_cmd_args():
     if len(sys.argv) == 3:
         try:
             with open(sys.argv[1], "r") as file:
-                data = file.readline
+                data = file.readline()
                 if data:
                     return True
                 else:
                     return False
         except (FileNotFoundError, IndexError):
-            sys.exit("Could not read ", sys.argv[1])
+            sys.exit("Could not read " + sys.argv[1])
 
 
 def read_csv(file):
-    """Reads the csv in sys.argv[1]
-    And returns 2 lists based on the csv header one for each column
-    """
-    with open(file, 'r') as csv_file:
+    """Reads the CSV file specified in sys.argv[1]
+    and performs operations on the data"""
+    with open(file, "r") as csv_file:
         csv_reader = csv.DictReader(csv_file)
-        rows = list(csv_reader) # read all rows into a list of dictionaries
-        
+        rows = list(csv_reader)  # read all rows into a list of dictionaries
+
         for row in rows:
             # Access the value of the first column using its header name
-            value = row['name']
-            
-            #perform replace operation
-            new_value = value.replace(', ', '')
-            
-            # update the value in the row
-            row['name'] = new_value
-            
-    # write the modified rows back to the csv file
-    fieldnames = csv_reader.fieldnames # get the orignal feildnames
-    with open(sys.argv[2], 'w', newline='') as file:
-        writer = csv.DictWriter(file, fieldnames=fieldnames)
+            value = row.pop("name")
+
+            # Split the name into first name and last name
+            first_name, last_name = value.split(" ", 1)
+            row["first_name"] = first_name.replace(",", "")
+            row["last_name"] = last_name
+
+    # Write the modified rows back to the CSV file
+    fieldnames = ["first_name", "last_name", "house"]  # Updated fieldnames
+    with open(sys.argv[2], "w", newline="") as file:
+        writer = csv.DictWriter(
+            file, fieldnames=fieldnames, quoting=csv.QUOTE_NONE, escapechar="\\"
+        )
         writer.writeheader()
         writer.writerows(rows)
+
 
 if __name__ == "__main__":
     main()
