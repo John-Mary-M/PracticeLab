40f5585cd3d44d66ef970260839ca84d18b59da7
John-Mary-M <johnmaryluxin8@gmail.com>
2023-10-24 19:30:24 +0300

    bugs fixxed

--- cs50P/shirt/shirt.py
+++ cs50P/shirt/shirt.py
@@ -1,12 +1,15 @@
 """cs50p 18/10/2023 FILE I/O problemset 6 shirt.py"""
 
 import sys
+import os
 from PIL import Image, ImageOps
+
 
 
 def main():
     """Entry point"""
     check()
+    overylay(sys.argv[1], sys.argv[2])
 
 
 def check():
@@ -16,17 +19,35 @@ def check():
         sys.exit("Too few command-line arguments")
     if len(sys.argv) > 3:
         sys.exit("Too many command-line arguements")
-    if (
-        not str(sys.argv[1], sys.argv[2]).endswith("png")
-        or str(sys.argv[1], sys.argv[2]).endswith("jpg")
-        or str(sys.argv[1], sys.argv[2]).endswith("jpeg")
-    ):
-        sys.exit("Input and output have different extensions")
-    if not sys.argv[1]:
+    # if (
+    #     not sys.argv[1].endswith("png")
+    #     or not sys.argv[1].endswith("jpg") or not sys.argv[1].endswith("jpeg")
+    # ):
+    #     sys.exit("Input and output have different extensions")
+    file_1 = os.path.splitext(sys.argv[1])
+    print(file_1)
+    file_2 = os.path.splitext(sys.argv[2])
+    print(file_2)
+    if extension_chk(file_1) is False:
         sys.exit("Ivalid input")
-    return True
+    if extension_chk(file_2) is False:
+        sys.exit("Ivalid output")
+    if file_1[1].lower() != file_2[1].lower():
+        sys.exit("Iput and output have different extensions")
+    # if (
+    #     not sys.argv[2].endswith("png")
+    #     or not sys.argv[2].endswith("jpg") or not sys.argv[2].endswith("jpeg")
+    # ):
+    #     sys.exit("Input and output have different extensions")
+    # if not sys.argv[1]:
+    #     sys.exit("Ivalid input")
+    # return True
 
-
+def extension_chk(file):
+    """checks file extension"""
+    if file[1] in [".jpg", ".jpeg", ".png"]:
+        return True
+    return False
 def overylay(image_in, image_out):
     """Takes the two commandline arguments as
     image_in which is sys.argv[1] and overlays it on
@@ -34,22 +55,19 @@ def overylay(image_in, image_out):
     Returns a new image that is a combination of the two
     command-line arguements
     """
-    # try:
-    size = (211, 211)
-    # open the base image
-    base_image = Image.open(image_in)
-    # Open the overlay
-    overlay = Image.open("shirt.png")
-    # Resize overlay image to the default size
-    overlay = ImageOps.fit(overlay, size, method=0, bleed=0.0, centering=0)
-    # Composite the overlay image on top of the base image
-    base_image.paste(overlay, (0, 0), overlay)
-    # Save the result as the output image
-    base_image.save(image_out)
-    return image_out
-    # except Exception:
-    #     pass
-
+    try:
+        image_file = Image.open(image_in)
+    except FileNotFoundError:
+        sys.exit("Input does not exist")
+    shirt_image = Image.open("shirt.png")
+    # get shirt size
+    size = shirt_image.size
+    # resize the muppet
+    muppet =ImageOps.fit(image_file, size)
+    # paste
+    muppet.paste(shirt_image, shirt_image)
+    # save new image
+    muppet.save(image_out)
 
 if __name__ == "__main__":
     main()
