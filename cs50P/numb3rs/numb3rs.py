f4cff445902ecc464b6d55e3f15ba280dae0e2fb
John-Mary-M <johonmaryluxin8@gmail.com>
2023-11-02 14:50:08 +0300

    Made changes in numb3rs.py and fixed all bugs

--- cs50P/numb3rs/numb3rs.py
+++ cs50P/numb3rs/numb3rs.py
@@ -4,21 +4,23 @@ import re
 
 
 def main():
+    """Entry point"""
     print(validate(input("IPv4 Address: ")))
 
 
 def validate(ip):
-    """Validates IPV4 IP address"""
-    # ip = sys.argv(1)
-    if re.search(r"^[0-9]+\.[0-9]+\.[0-9]+\.[0-9]$", ip):
+    """Validates IPv4 IP address"""
+    match = re.search(r"^(\d{1,3}\.){3}\d{1,3}$", ip)
+    if match:
         lst_prts_ip = ip.split(".")
         for prt in lst_prts_ip:
-            if int(prt) < 0 or int(prt) > 255:
+            if 0 <= int(prt) <= 255:
+                continue
+            else:
                 return False
         return True
     else:
         return False
-
 
 if __name__ == "__main__":
     main()
--- cs50P/numb3rs/test__numb3rs.py
+++ cs50P/numb3rs/test__numb3rs.py
@@ -1,20 +1,30 @@
 """pytest for validate(ip) in numb3rs.py"""
+import subprocess
 from numb3rs import validate
 
 
 def main():
     test_range()
-
+    test_invalid()
+    test_firstByte_in_range()
+    test_exit_code()
 
 def test_range():
     """Tests ipv4 range"""
-    assert validate("192.168.0.3")
-    assert validate("1.2.3.4")
+    assert validate("192.168.0.3") is True
+    assert validate("255.255.255.255") is True
 
+def test_firstByte_in_range():
+    """checks octets with one digit"""
+    assert validate("192.278.3258.496") is False
 
 def test_invalid_range2():
     """tests invalid range"""
-    assert validate("279.198.4587.3") is False
+    assert validate("279.198.458.278") is False
+
+def test_firstByte_out_of_range():
+    """Tests only the first byte of IPv4 address is in range"""
+    assert validate("256.168.0.3") is False
 
 
 if __name__ == "__main__":
