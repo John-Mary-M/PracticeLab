dc3d3ef8e787ec9ef1767614b1c00952e7bd9af7
John-Mary-M <johnmaryluxin8@gmail.com>
2023-11-05 06:43:01 +0300

    modified regex to capture group by group and wrote more tests

--- cs50P/working/test_working.py
+++ cs50P/working/test_working.py
@@ -1,18 +1,42 @@
 """pytests for working.py"""
+from _pytest.junitxml import pytest
+from pytest import raises
 from working import convert
+
 
 def main():
     test_day_time_1()
     test_day_time_2()
     test_night_time_1()
     test_night_time_2()
+
+
 def test_day_time_1():
     assert convert("9:00 AM to 5:00 PM") == "09:00 to 17:00"
+
+
 def test_day_time_2():
     assert convert("9 AM to 5 PM") == "09:00 to 17:00"
+
+
 def test_night_time_1():
     assert convert("5:00 PM to 9:00 AM") == "17:00 to 09:00"
+
+
 def test_night_time_2():
     assert convert("5 PM to 9 AM") == "17:00 to 09:00"
+
+
+def test_time_with_minutes():
+    assert convert("10:30 PM to 8:50 AM") == "22:30 to 08:50"
+
+
+def test_invalid_input():
+    with pytest.raises(ValueError):
+        convert("9:60 AM to 5:60 PM")
+        convert("9 AM - 5 PM")
+        convert("09:00 AM - 17:00 PM")
+
+
 if __name__ == "__main__":
     main()
--- cs50P/working/working.py
+++ cs50P/working/working.py
@@ -1,8 +1,6 @@
 """cs50p 4/11/2023 problemset 7 working.py time converter function"""
 import re
 
-# import sys
-
 
 def main():
     """Entry point"""
@@ -12,28 +10,30 @@ def main():
 def convert(s):
     """coverts 12 hour clock time
     to 24 hour clock time"""
-    match = re.search(r"^([0-9])(:?\d*\ \w*\ to\ )([0-9])(\ ?:?\d*\ \w*)", s)
+    match = re.search(
+        r"(\d+)(?::(\d+))?\s*(AM|PM)\s*to\s*(\d+)(?::(\d+))?\s*(AM|PM)", s
+    )
     if not match:
         raise ValueError
-    # check the arguements and prefix 0 or add 12
-    if "AM" in match.group(2):
-        hr_1 = "0" + match.group(1)
-    elif "PM" in match.group(2):
-        hr_1 = int(match.group(1)) + 12
-    # check for PM and add 12
-    if "PM" in match.group(4):
-        hr_2 = int(match.group(3)) + 12
-    elif "AM" in match.group(4):
-        hr_2 = "0" + match.group(3)
-    # format the result
-    converted_time = f"{hr_1}{match.group(2)}{hr_2}{match.group(4)}"
-    converted_time = re.sub(r"AM", "", converted_time)
-    converted_time = re.sub(r"PM", "", converted_time)
-    # conditionally remove the AM and PM and add the minutes
-    if re.search(r"\w*", match.group(2)) and re.search(r"\w*", match.group(4)):
-        converted_time = f"{hr_1}:00 to {hr_2}:00"
+    hr_1 = int(match.group(1))
+    min_1 = int((match.group(2)) or "00")  # Use 0 if minutes are not provided
+    if min_1 >= 60:
+        raise ValueError
+    am_pm_1 = match.group(3)
+    hr_2 = int(match.group(4))
+    min_2 = int((match.group(5)) or "00")
+    if min_2 >= 60:
+        raise ValueError
+    am_pm_2 = match.group(6)
 
+    # AM PM conversions
+    if am_pm_1 == "PM" and hr_1 < 12:
+        hr_1 += 12
+    if am_pm_2 == "PM" and hr_2 < 12:
+        hr_2 += 12
+    converted_time = f"{hr_1:02}:{min_1:02} to {hr_2:02}:{min_2:02}"
     return converted_time
+
 
 if __name__ == "__main__":
     main()
