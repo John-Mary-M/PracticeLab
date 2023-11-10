02cf9303646dc5bd8c1bf4943b363477bb49acac
John-Mary-M <johnmaryluxin8@gmail.com>
2023-11-10 05:56:52 +0300

    started on seasons.py

---
+++ cs50P/seasons/seasons.py
@@ -0,0 +1,23 @@
+"""cs50p problemset 8 10/11/2023
+     program that prompts the user for their date of birth in YYYY-MM-DD
+     format and then rints how old they are in minutes, rounded to the 
+     nearest integer, using English words instead of numerals,  without 
+     any and between words. Since a user might not know the time at which 
+     they were born, assume, for simplicity, that the user was born at midnight 
+     (i.e., 00:00:00) on that date. And assume that the current time is also 
+     midnight. In other words, even if the user runs the program at noon, 
+     assume that it’s actually midnight, on the same date
+"""
+from datetime import date
+
+
+def main():
+    """Entry Point"""
+    ...
+
+
+...
+
+
+if __name__ == "__main__":
+    main()
