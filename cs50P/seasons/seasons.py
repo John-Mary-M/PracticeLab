8e3d6c38e09f78f28a72c1f5fba8c267bc46e3a5
John-Mary-M <johnmaryluxin8@gmail.com>
2023-11-11 05:32:42 +0300

    minute counter updated to an OOP implementation

--- cs50P/seasons/seasons.py
+++ cs50P/seasons/seasons.py
@@ -8,16 +8,60 @@
      midnight. In other words, even if the user runs the program at noon, 
      assume that it’s actually midnight, on the same date
 """
+import sys
 from datetime import date
+import inflect
 
+
+class DateUtils:
+    '''Has functionalities that enable minute counting from birt till date'''
+    def __init__(self, birth_date):
+        self.birth_date = birth_date
+
+    def calculate_minutes_lived(self):
+        '''counts the total minutes lived till date'''
+        current_date = date.today()
+        year_difference = current_date.year - self.birth_date.year
+
+        total_days = year_difference * 365  # Approximation for simplicity
+
+        for year in range(self.birth_date.year, current_date.year):
+            if self.is_leap_year(year):
+                total_days += 1  # Add an extra day for leap years
+
+        total_minutes = total_days * 24 * 60  # Convert days to minutes
+
+        return total_minutes
+
+    @staticmethod
+    def is_leap_year(year):
+        '''checks for leap year'''
+        return (year % 4 == 0 and year % 100 != 0) or (year % 400 == 0)
+
+class WordsConverter:
+    '''Converts minutes from int to words'''
+    @staticmethod
+    def convert_to_words(minutes):
+        p = inflect.engine()
+        return p.number_to_words(int(round(minutes)))
+
+def get_date():
+    '''Gets date of birth from the user'''
+    try:
+        birth_day_str = input("Date of Birth (YYYY-MM-DD): ")
+        year, month, day = map(int, birth_day_str.split('-'))
+        return date(year, month, day)
+    except ValueError:
+        sys.exit('Ivalid date')
 
 def main():
-    """Entry Point"""
-    ...
-
-
-...
-
+    '''Entry Point'''
+    birthday = get_date()
+    if birthday:
+        date_util = DateUtils(birthday)
+        minutes_lived = date_util.calculate_minutes_lived()
+        minutes_in_words = WordsConverter.convert_to_words(minutes_lived)
+        print(f"{minutes_in_words}")
 
 if __name__ == "__main__":
     main()
